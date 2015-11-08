d50cb77e01c69328d11a09a2e73f369523df5a45
Guo chen <albertguosgp@gmail.com>
2015-11-08 10:15:05 +0800

    clustered.

--- src/main/java/flextrade/flexvision/fx/config/VertxConfig.java
+++ src/main/java/flextrade/flexvision/fx/config/VertxConfig.java
@@ -1,20 +1,53 @@
 package flextrade.flexvision.fx.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class VertxConfig {
-    @Bean
-    public EventBus createEventBus() {
-        return createVertx().eventBus();
-    }
+    private Vertx vertx;
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
 
     @Bean
-    public Vertx createVertx() {
-        return Vertx.vertx();
+    @DependsOn(value = "vertx")
+    public EventBus createEventBus() {
+        return vertx.eventBus();
+    }
+
+    @Bean(name = "vertx")
+    public Vertx createVertx() throws InterruptedException {
+        ClusterManager clusterManager = new HazelcastClusterManager();
+        VertxOptions options = new VertxOptions().setClusterManager(clusterManager);
+
+        Vertx.clusteredVertx(options, res -> {
+            if (res.succeeded()) {
+                vertx = res.result();
+                countDownLatch.countDown();
+                log.info("Clustered vertx created successfully" + vertx);
+            } else {
+                log.error("Failed: " + res.cause());
+            }
+        });
+        boolean isVertxClustered = countDownLatch.await(30, TimeUnit.SECONDS);
+        if (!isVertxClustered) {
+            throw new RuntimeException("Failed to vertx cluster");
+        }
+        return vertx;
     }
 }
